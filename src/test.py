f2679761e8304c57ed23d46fea674fe02d554d94
Lesh79 <Temerlan.axmetov.75@mail.ru>
2023-11-24 19:01:24 +0300

    updated test

--- src/test.py
+++ src/test.py
@@ -6,8 +6,8 @@ class test_GraphCPD(unittest.TestCase):
 
     def test_graph1(self):
         data_set1 = [50, 55, 60, 48, 52, 70, 75, 80, 90, 85, 95, 100, 50]
-        graph1 = GraphCPD(data_set1,5,3)
-        Rg = graph1.check_edges_existence()
+        graph1 = GraphCPD(data_set1,5)
+        Rg = graph1.check_edges_existence(3)
         self.assertEqual(Rg, 5, "Rg(t) is not true")
 
         numOfEdges = graph1.calculateEdges()
@@ -16,16 +16,16 @@ class test_GraphCPD(unittest.TestCase):
         sum_squares_degree = graph1.sumOfSquaresOfDegreesOfNodes()
         self.assertEqual(sum_squares_degree, 96, "Sum of squares of degree  is not true")
 
-        e = graph1.calculation_E()
+        e = graph1.calculation_E(3)
         self.assertEqual(e, 6.153846153846154, "E(Rg(t)) is not true")
 
-        var = graph1.calculation_Var()
+        var = graph1.calculation_Var(3)
         self.assertEqual(var, 2.7455621301775164, "Var(Rg(t)) is not true")
 
-        z = graph1.calculation_z()
+        z = graph1.calculation_z(3)
         self.assertEqual(z, 0.6963575181639445, "Zg(t) is not true")
 
-        self.assertEqual(graph1.find_changepoint(0.6), "Change point", "IDI NAHUI NICHEGO NE IZMENILOS")
+        self.assertEqual(graph1.find_changepoint(1), 5, "Incorrect change point")
 
     # def test_graph2(self):
     #     data_set1 = [20, 22, 25, 24, 23, 26, 28, 32, 35, 36, 38, 37, 35,34, 33,  ]
